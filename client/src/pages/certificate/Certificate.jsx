b7574a3a12478ea045ba6c3477f0aece6cfb0ac5
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2025-09-17 16:35:39 +0530

    remove 1 same certificate

--- client/src/pages/certificate/Certificate.jsx
+++ client/src/pages/certificate/Certificate.jsx
@@ -8,17 +8,10 @@ const certificates = [
     id: 1,
     name: "MERN Stack Developer",
     pdf: "/pdf/Mern_stack.pdf",
-    issueDate: "June 2025",
+    issueDate: "August 2025",
     description: "Completed a full MERN stack course with practical projects.",
   },
-  {
-    id: 2,
-    name: "Full Stack Developer",
-    pdf: "/pdf/Mern_stack.pdf",
-    issueDate: "July 2025",
-    description:
-      "Advanced full-stack development certification with React & Node.js.",
-  },
+ 
 ];
 
 const Certificate = () => {
